8f80fb8b2a18a11e258cf746fd8ace7c1616904a
Faza Nurfai'zi <nurfaizi.faza@gmail.com>
2020-09-11 22:27:26 +0700

    Setup Webpack

---
+++ webpack.prod.config.js
@@ -0,0 +1,48 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, 'src', 'index.js')
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    modules: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader', 'eslint-loader'
+                ]
+            },
+            {
+                test: /\.s(c|a)ss$/,
+                use: [
+                    'style-loader', 'css-loader', 'sass-loader', 'scss-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            fetch: 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch'
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        })
+    ]
+};
